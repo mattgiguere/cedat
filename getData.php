55cbd1d6dd2d187f17eb2190dc6c145a7f0bfdf3
Matt Giguere <matthew.giguere@gmail.com>
2014-07-08 17:51:57 -0400

    retrieving data with php now works

--- getData.php
+++ getData.php
@@ -1,6 +1,66 @@
 <?php
-$afspath = $_SERVER["AeroFSdir"];
-$credsfile = $afspath . '.credentials/SQL/csaye';
-$file = file_get_contents($credsfile);
-echo $file;
+    $afspath = $_SERVER["AeroFSdir"];
+    $credsfile = $afspath . '.credentials/SQL/csaye';
+    $file = file_get_contents($credsfile);
+    //echo "The host is: ";
+    //echo $file[0];
+    echo $file;
+
+    $creds = explode("\n", $file);
+    //echo $creds[0];
+    //echo "\n ";
+
+    $host = $creds[0];
+    $port = $creds[1];
+    $username = $creds[2];
+    $password = $creds[3];
+    $database = $creds[4];
+
+    /*
+    echo "host is:";
+    echo $host;
+    echo "\n";
+    echo "username is: ";
+    echo $username;
+    echo "\n";
+    echo "password is: ";    
+    echo $password;
+    echo "\n";
+    */
+
+
+    $server = mysql_connect($host, $username, $password);
+    echo $server;
+    echo "\n";
+    $connection = mysql_select_db($database, $server);
+    echo $connection;
+    echo "\n";
+
+    $myquery = "
+    SELECT v.jd, v.mnvel FROM velocities v INNER JOIN observations o ON  o.observation_id=v.observation_id WHERE o.object='10700';
+    ";
+    /*
+    $myquery = "
+    SELECT  `date`, `close` FROM  `data2`
+    ";
+    */
+    
+
+    $query = mysql_query($myquery);
+    
+    if ( ! $query ) {
+        echo mysql_error();
+        die;
+    }
+    
+    $data = array();
+    
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+    
+    echo json_encode($data);     
+     
+    mysql_close($server);
+
 ?>
