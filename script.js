baae5dc557e4f9b87911a76bb92e6f0a755fc945
Matt Giguere <matthew.giguere@gmail.com>
2014-08-29 17:02:30 -0400

    incorporated ajax
    script.js now uses the ajax jquery method to grab the new data using php

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('.velocities').click(function() {
+        //alert("Clicked!");
+        update($(this).text());
+    });
+    $('.velocities').mouseenter(function() {
+        $(this).fadeTo('fast', 1.0);
+    });
+    $('div').mouseleave(function() {
+        $('.velocities').fadeTo('fast', 0.85);
+    });
+});
+
+/*
+        $.ajax({
+            type: "POST", 
+            url: "php/getNewData.php", 
+            data: { param: $(this).text(); }
+        },
+        success: function( data ) {
+            console.log(data);
+        }
+        });
+        });
+
+
+$(function (){
+   $("#myimg").click(function() {
+      $.ajax({
+        type: "POST",
+        url: "some.php",
+        data: { param: $(this).attr('src'); }
+      }).done(function( msg ) {
+             alert( "Data Saved: " + msg );
+     });
+  });
+}
+
+    $('button').click(function() {
+    });
+
+
+*/
