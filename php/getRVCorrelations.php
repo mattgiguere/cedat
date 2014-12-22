4ee3a69f535ca24114ba8d6c4e7d97736a892c2a
Matt Giguere <matthew.giguere@gmail.com>
2014-12-22 11:06:59 -0500

    added getRVCorrelations.php
    a page to return the information to plot RV correlations

---
+++ php/getRVCorrelations.php
@@ -0,0 +1,113 @@
+<?php
+    //$afspath = $_SERVER["AeroFSdir"];
+    $pltpar = $_GET['param'];
+
+    if (gethostname() == 'aramis.astro.yale.edu') {
+        $afspath = "/mg/AeroFS/";    
+    } else {
+        $afspath = "/Users/matt/AeroFS/";
+    }
+    
+    //  
+    //echo $afspath;
+    $credsfile = $afspath . '.credentials/SQL/csaye';
+    $file = file_get_contents($credsfile);
+    //echo "hello";
+    //echo "The host is: ";
+    //echo $file[0];
+    //echo $file;
+    
+    $creds = explode("\n", $file);
+    //echo $creds[0];
+    //echo "\n ";
+
+    $host = $creds[0];
+    $port = $creds[1];
+    $username = $creds[2];
+    $password = $creds[3];
+    $database = $creds[4];
+
+    
+    $server = mysql_connect($host, $username, $password);
+    //echo $server;
+    //echo "\n";
+    $connection = mysql_select_db($database, $server);
+    //echo $connection;
+    //echo "\n";
+
+    $whereclause = ' WHERE ';
+    $usewhere = 0;
+
+    if (isset($_GET['objectnm'])){
+        $objectnm = $_GET['objectnm'];
+        $whereclause .= 'o.object = ' . $objectnm;
+        $usewhere = 1;
+        //echo "whereclause is: ";
+        //echo $whereclause;
+    }
+
+    if (isset($_GET['hasvels'])){
+        if ($usewhere == 1){
+            $whereclause .= ' AND v.mnvel IS NOT NULL';
+        } else{
+            $usewhere = 1;
+            $whereclause .= ' v.mnvel IS NOT NULL';
+        }
+
+        
+    }
+    if (isset($_GET['tablenm'])) {
+        $sqltblnm = $_GET['tablenm'];
+        //echo "tablenm was set. The value is: ";
+        //echo $sqltblnm;
+    } else {
+        $myquery = "
+        SELECT v.mnvel, v.errvel, v.mdchi FROM observations o INNER JOIN velocities v ON o.observation_id = v.observation_id";
+    }
+            
+
+
+    if ($sqltblnm =='observations') {
+        $myquery = "
+    SELECT o.date_obs as date, o." . $pltpar . " as xdata, o.obnm as obnm, o.object as objectnm FROM velocities v INNER JOIN observations o ON  o.observation_id=v.observation_id WHERE o.object='" . $objectnm . "' AND o.date_obs != 'date-obs' AND v.mnvel IS NOT NULL ORDER BY o.date_obs DESC;
+    ";   
+    }
+
+    if ($sqltblnm =='velocities') {
+        $myquery = "
+    SELECT o.date_obs as date, v." . $pltpar . " as ydata, o.obnm as obnm, o.object as objectnm, sqrt(v.cts) as snr, o.exptime as exptime, o.zd as zd FROM velocities v INNER JOIN observations o ON  o.observation_id=v.observation_id WHERE o.object='" . $objectnm . "' AND o.date_obs != 'date-obs' AND v.mnvel IS NOT NULL ORDER BY o.date_obs ASC;
+    ";   
+    }
+    
+    /*
+    if (($sqltblnm !='observations') and ($sqltblnm !='velocities')) {
+        $myquery = "
+        SELECT o.date_obs as date, ". $sqltblnm . "." . $pltpar . " as ydata, o.obnm as obnm, o.object as objectnm FROM observations o INNER JOIN " . $sqltblnm . " ON " . $sqltblnm.  ".observation_id=o.observation_id WHERE o.object='" . $objectnm . "' AND ". $sqltblnm . "." . $pltpar . " IS NOT NULL AND o.date_obs != 'date-obs' ORDER BY o.date_obs DESC;";   
+    }
+    */
+
+    if ($usewhere == 1) {
+        $myquery = $myquery . $whereclause;
+    }
+
+    //echo $myquery;
+
+    $query = mysql_query($myquery);
+    
+    if ( ! $query ) {
+        echo mysql_error();
+        die;
+    }
+    
+    $data = array();
+    
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+    
+    echo json_encode($data);     
+     
+    mysql_close($server);
+    //*/
+
+?>
